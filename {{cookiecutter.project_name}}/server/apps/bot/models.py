461db02d06c575c153ba3ec7f4098a6cf5fd86bc
Yarfuo <yarfuo@gmail.com>
2021-05-04 20:44:59 +0300

    Changed `last_name` field options

--- {{cookiecutter.project_name}}/server/apps/bot/models.py
+++ {{cookiecutter.project_name}}/server/apps/bot/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class TelegramUser(models.Model):
     telegram_user_id = models.IntegerField(unique=True)
     first_name = models.CharField(max_length=512)
-    last_name = models.CharField(max_length=512)
+    last_name = models.CharField(max_length=512, null=True, blank=True)
     username = models.CharField(max_length=32, null=True, blank=True)
     language_code = models.CharField(max_length=8, null=True, blank=True)
 
